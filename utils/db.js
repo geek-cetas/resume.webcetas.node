c9acf17e97aef30341f3ceeea702fe23307379f2
kailash <retadedgeek@gmail.com>
2011-04-11 21:37:44 +0530

    Revert "Revert "Added utils folder which will be used by the application""
    This reverts commit 75234a575a4e33d619463cbd2385e5a72641fda5.

---
+++ utils/db.js
@@ -0,0 +1,31 @@
+
+Common = { resume : { Name : 'Kailashnath Reddy',
+                      Mailid : 'kailashnathreddy@ymail.com',
+                      SocialNetworks : { Twitter : 'http://twitter.com/kailashnath_k',
+                                         Facebook : 'http://facebook.com/kailashnath.kavalakuntla',
+                                         LinkedIn : 'http://in.linkedin.com/in/kailashnathreddy', 
+                                       },
+                      Description : "A geek by nature, programmer by passion and an ardinant fan of steve jobs",
+                      Skills : { Technical : { Languages : ['c', 'java', 'nodejs', 'python', 'C#'],
+                                               Web       : ['html', 'javascript', 'ajax'],
+                                               OS        : ['Linux', 'Windows'],
+                                               Scripting : ['shell', 'python'],
+                                               Database  : ['mysql', 'couchdb']
+                                             },
+                                 Managerial : ["Can lead a team and be a great team player",
+                                               "Enlighten the clients on different technologies"]
+                                },
+                      Projects : [{ Company : 'Infosys technologies limited',
+                                    Client  : 'British telecom',
+                                    Technologies : ['python', 'java', 'c#.net', 'php', 'j2ee', 'oracle 9i', 'RHEL'],
+                                    Duration : '24 months'},
+                                  { Company : 'Infosys technologies limited',
+                                    Client  : 'Airtel digital tv',
+                                    Technologies : ['python', 'django', 'mysql', 'apache', 'php', 'Open X Adserver'],
+                                    Duration : '8 months'}
+                                 ]
+                    }
+         };
+
+module.exports = Common;
+
