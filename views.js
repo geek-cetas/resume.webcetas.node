6b5012ecd3661269ed7b43058681236b09a9a9e4
kailash <retadedgeek@gmail.com>
2011-04-11 21:33:37 +0530

    Revert "Added the concept of views". Reason : swap file of db.js added
    This reverts commit 972940551e366cab51e7510b183db8723ff30907.

--- views.js
+++
@@ -1,8 +0,0 @@
-var db = require('./utils/db');
-var tr = require('./utils/format').tr;
-var ops = require('./views/operations');
-
-views = { home : ops.home,
-          resume : ops.resume }
-
-module.exports = views;
