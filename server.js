aab3e1ba4f90d8e8e69177716acd236794d3f2c1
Kailash <kailashnathreddy@ymail.com>
2011-04-11 11:37:08 +0530

    Port changed to 8080

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ var http = require('http');
 http.createServer( function( req, res ) {
     res.writeHead( 200, {'Content-Type' : 'text/plain'} );
     res.end( 'Hello world \n' );
-    }).listen();
+    }).listen( 8080 );
 console.log( 'Server started successfully' ); 
