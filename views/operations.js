bcaaec2780217362ff26dbc5697a868c9e91a1e2
Kailash <kailashnathreddy@ymail.com>
2011-04-12 13:39:11 +0530

    Added handler for '/'

--- views/operations.js
+++ views/operations.js
@@ -1,7 +1,7 @@
 var tr = require('../webcetas.node/utils/format').tr;
 var db = require('../utils/db');
 
-url_mapping = { '/home' : home, '/resume' : resume };
+url_mapping = { '/' : resume, '/home' : home, '/resume' : resume };
 
 function home( req, res )
 {
