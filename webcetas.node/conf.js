a07a532f895ab4e3d5cd1f40fd80a8d6323a9f80
kailash <retadedgeek@gmail.com>
2011-04-15 23:04:37 +0530

    Removed webcetas.node as it has been moved into different directory

--- webcetas.node/conf.js
+++
@@ -1,5 +0,0 @@
-Config = { port : 80,
-           views : {},
-           translate_engine : function( dst, text, callback ) { console.log('still here'); return callback( text ); } }
-
-module.exports = Config
