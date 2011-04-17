6147c842f7407d1827762c62b24a2e393c59929a
kailash <retadedgeek@gmail.com>
2011-04-17 22:15:35 +0530

    Code formatting

--- views/operations.js
+++ views/operations.js
@@ -1,50 +1,43 @@
-var fmt = require('../webcetas.node/utils/formats').fmt;
+var fmt = require('../utils/formats').fmt;
 var view = require('../webcetas.node/views').view;
 var db = require('../utils/db');
 var fs = require('fs');
-
 
 view('/home/([a-z]+).html',
 function home( req, res, page )
 {
     var out = res;
     var $ = this;
-    fs.readFile('./views/html/' + page +'.html', function( err, data )
+    fs.readFile( './views/html/' + page +'.html', function( err, data )
     {
         if( err ) throw err;
-        out.write(data);
+        out.write( data );
         out.end();
     });
 });
 
-view('^(/|/resume)$',
+view('^(/|/resume)[/]?$',
 function resume( req, res, args )
 {
-    res.writeHead(200, {'Content-type' : 'text/plain;charset=utf-8'});
+    res.writeHead( 200, {'Content-type' : 'text/plain;charset=utf-8'} );
     fmt( db.resume, write( res ));
     res.end();
 });
 
-view('/skills$',
-function skills( req, res )
+view('/resume/([a-zA-Z]+)[/]?$',
+function sub( req, res, args )
 {
-    res.writeHead(200, {'Content-type' : 'text/plain;charset=utf-8'});
-    fmt( db.resume.Skills, write( res ));
-    res.end();
-});
-
-view('/blog$',
-function blog( req, res )
-{
-    res.writeHead( 302, {'Location' : 
-                            'http://www.kailashnath.posterous.com'} );
+    var key = args[0].charAt(0).toUpperCase() + args[0].slice(1);
+    console.log( key );
+    res.writeHead( 200, {'Content-type' : 'text/plain;charset=utf-8'} );
+    fmt( db.resume[key], write( res ));
     res.end();
 });
 
 view('/vtest/([0-9]+)/([0-9]+)/$',
     function vtest( req, res, args )
     {
-        res.end("kailash test success !!" + args);
+        res.end( "kailash test success !!" + args );
     });
 
 function write( res ) 
